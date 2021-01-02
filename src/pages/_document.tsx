39b82a7b73ba80abd30383954f45bbea7587882a
redshoga <yui.ichioka.contact@gmail.com>
2021-01-02 09:35:33 +0900

    不要なimportの削除

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,4 @@
-import Document, {
-  DocumentContext,
-  Html,
-  Head,
-  Main,
-  NextScript,
-} from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 import React from "react";
 import meta from "../../blog/config/meta.json";
 
